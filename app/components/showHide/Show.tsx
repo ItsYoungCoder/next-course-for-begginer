336aedc0320f409b782f148dc5d97159e4108411
CodeAmboka <ambokadzedato0@gmail.com>
2024-05-25 17:20:45 +0400

    add show/hide component

---
+++ app/components/showHide/Show.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { useState } from 'react'
+import styles from './show.module.scss'
+export default () => {
+
+    const [show, setShow] = useState(false)
+
+
+    const onClick = () => {
+        setShow(!show)
+    }
+    return (
+        <div>
+            <button className={styles.button} onClick={onClick}>{show ? 'Hide Menu' : 'Show Menu'}</button>
+            {show && <ul className={styles.listContainer}>
+                        <li className={styles.listItems}>Profile</li>
+                        <li className={styles.listItems}>Subscription</li>
+                        <li className={styles.listItems}>Settings</li>
+                        <li className={styles.listItems}>Log Out</li>
+                    </ul>
+            }
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Button from "./components/button/Button";
+import Show from "./components/showHide/Show";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Button />
+      <Show />
     </main>
   );
 }
